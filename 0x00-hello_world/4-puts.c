fa138c938cb4534f693b8cd3350693b451e30ca2
jasonkim64 <jk1746823@gmail.com>
2023-07-13 13:03:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - Aprogram that prints a line with puts function
  * n Return: 0
  */
-int main(void) 
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-} 
+}
