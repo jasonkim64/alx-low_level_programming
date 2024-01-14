9125fe453e605f3009d409efce69abb5000f5a91
samuel <jk1746823@gmail.com>
2024-01-14 14:16:40 -0800

    100-change.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: number of argurment
+ * @argc: array of arguments
+ *  Return: Always 0 (Success)
+
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+
+{
+
+	printf("%s\n", *argv);
+
+
+
+	return (0);
+
+}
+
